4586cd083807930b14403f34f007a26f579361e5
ryanhamley <ryan.hamley@mapbox.com>
2019-02-03 22:57:04 -0800

    Remove ES6

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const shrinkagePercentages = {
+var shrinkagePercentages = {
   porcelain: 13.9,
   stoneware: 11.8
 };
 
-const ceramicBody = document.getElementById('ceramic-body');
-const size = document.getElementById('size');
-const outputContainer = document.getElementById('output-container');
-const output = document.getElementById('output');
+var ceramicBody = document.getElementById('ceramic-body');
+var size = document.getElementById('size');
+var outputContainer = document.getElementById('output-container');
+var output = document.getElementById('output');
 
-const calculateBtn = document.getElementById('calculateBtn');
-calculateBtn.addEventListener('click', (e) => {
-  const originalSize = ((size.value / (100 - shrinkagePercentages[ceramicBody.value])) * 100).toFixed(2);
+var calculateBtn = document.getElementById('calculateBtn');
+calculateBtn.addEventListener('click', function (e) {
+  var originalSize = ((size.value / (100 - shrinkagePercentages[ceramicBody.value])) * 100).toFixed(2);
   output.innerHTML = originalSize;
 });
